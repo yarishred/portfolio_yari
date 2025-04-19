d3c0da2556f9d7b15571add727a6d26f2465baf2
Yari Ruiz <yfruiz9@gmail.com>
2025-04-18 23:10:48 -0500

    feature: implement About section styling with profile image
    - Added custom styling for the About section layout
    - Created decorative shape with CSS transforms and pseudo-elements
    - Added circular figure container with shadow effects for profile image
    - Implemented responsive positioning with transform translate

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import aboutMe from "../../assets/images/global/aboutme.png";
+
 // Import Styles
 import "../../styles/components/AboutSection.css";
 
@@ -7,6 +9,12 @@ export const AboutSection = () => {
   return (
     <section className="layout__about">
       <h1>About Section</h1>
+
+      <div className="layout__about__shape">
+        <figure>
+          <img src={aboutMe} alt="" />
+        </figure>
+      </div>
     </section>
   );
 };
